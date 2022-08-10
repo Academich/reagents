ab43b8bbe259826fb91322f54f89869222af2518
Academich <mihandronov@gmail.com>
2022-08-10 16:36:44 +0200

    Made imports in `prepare_data.py` prettier

---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,3 @@
+from src.preprocessing.solvents import SOLVENTS
+from src.preprocessing.reaction_preprocessing import ReactionPreprocessingPipeline
+from src.preprocessing.reagents_classification import HeuristicRoleClassifier
