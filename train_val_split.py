dec711c269710845a54849730e9568aba6069ae9
Academich <mihandronov@gmail.com>
2022-09-14 11:34:10 +0200

    Added another argument

--- train_val_split.py
+++ train_val_split.py
@@ -16,6 +16,7 @@ parser = ArgumentParser()
 parser.add_argument("--path_data", type=str)
 parser.add_argument("--save_folder", type=str)
 parser.add_argument("--val_size", type=int)
+parser.add_argument("--output", type=str)
 args = parser.parse_args()
 
 uspto_full = pd.read_csv(args.path_data, sep="\t", usecols=["OriginalReaction"])
@@ -31,5 +32,5 @@ print("Validation: {} reactions.".format(uspto_val.shape[0]))
 if __name__ == '__main__':
     # === Saving ===
     save_path = Path(args.save_folder).resolve()
-    uspto_train.to_csv(save_path / "uspto_train.csv", index=False, columns=["OriginalReaction"], sep="\t")
-    uspto_val.to_csv(save_path / "uspto_val.csv", index=False, columns=["OriginalReaction"], sep="\t")
+    uspto_train.to_csv(save_path / f"{args.output}_train.csv", index=False, columns=["OriginalReaction"], sep="\t")
+    uspto_val.to_csv(save_path / f"{args.output}_val.csv", index=False, columns=["OriginalReaction"], sep="\t")
