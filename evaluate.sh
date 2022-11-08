6ed07d5a1f5d8ae17610a3a002b87589c978fab1
Academich <mihandronov@gmail.com>
2022-11-08 17:28:09 +0100

    Small mistake

--- evaluate.sh
+++ evaluate.sh
@@ -2,9 +2,9 @@ dataset=MIT_mixed
 model=${dataset}_model_step_20000.pt
 
 python translate.py -model experiments/checkpoints/${dataset}/${model} \
-                    -src data/${dataset}/src-test-perm.txt \
+                    -src data/${dataset}/src-test.txt \
                     -output experiments/results/predictions_${model}_on_${dataset}_test-perm.txt \
                     -batch_size 64 -replace_unk -max_length 200 -fast -beam_size 5 -n_best 5 -attn_debug
 
-python score_predictions.py -targets data/${dataset}/tgt-test-perm.txt \
+python score_predictions.py -targets data/${dataset}/tgt-test.txt \
                     -predictions experiments/results/predictions_${model}_on_${dataset}_test-perm.txt
