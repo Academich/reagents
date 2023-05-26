5f764cdafcd3258e84cd3fff64f9d6df2d113f88
mikhailandronov <mihandronov@gmail.com>
2023-05-26 13:33:22 +0200

    Removed whatever was left of onmt. ONMT is now supposed to be installed to the environment separately

--- test.sh
+++
@@ -1,4 +0,0 @@
-python translate.py -model experiments/trained_models/reag_simple_tok_with_rep_model_step_500000.pt \
-                    -src data/test/src-test-reagents.txt \
-                    -output experiments/results/predictions_on_test_reagents.txt \
-                    -batch_size 64 -replace_unk -max_length 200 -fast -beam_size 5 -n_best 5
